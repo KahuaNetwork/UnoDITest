30b80ac9f0b2ba63927a4688b458e09f87682221
Adam Love <ALove@Kahua.com>
2020-11-18 10:22:08 -0500

    Add project files.

---
+++ DITest/DITest.Shared/Locater.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DITest
+{
+    public static class Locator
+    {
+        public static void Initialize()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton(typeof(IService), typeof(Service));
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var service = serviceProvider.GetService(typeof(IService));
+        }
+    }
+}
---
+++ DITest/DITest.Wasm/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using Windows.UI.Xaml;
+
+namespace DITest.Wasm
+{
+    public class Program
+    {
+        private static App _app;
+
+        static int Main(string[] args)
+        {
+            Windows.UI.Xaml.Application.Start(_ => _app = new App());
+
+            return 0;
+        }
+    }
+}
